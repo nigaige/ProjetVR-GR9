ea9d38110cc39a2ae902bde788041923a12c25db
nigaige <nicolas.gaige@free.fr>
2023-02-22 12:56:58 +0100

    player

---
+++ Assets/Script/animateHandScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+public class animateHandScript : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float pinchValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", pinchValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+
+
+        Debug.Log(pinchValue);
+
+    }
+}
