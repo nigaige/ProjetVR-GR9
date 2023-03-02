a894d8762a60efd96223df22f11c79850c89922f
Morgan <bissonmorgan@gmail.com>
2023-03-02 11:59:55 +0100

    haptic interaction

--- Assets/Scripts/Turret/Missile/FireMissileOnActivate.cs
+++ Assets/Scripts/Turret/Missile/FireMissileOnActivate.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class FireMissileOnActivate : MonoBehaviour
 {
+    [SerializeField]
+    private InputActionProperty _indexTrigger; 
+
     [SerializeField]
     private GameObject _missile;
 
@@ -14,33 +18,79 @@ public class FireMissileOnActivate : MonoBehaviour
     [SerializeField]
     private float _fireSpeed;
 
+    private XRBaseController _leftHand;
+    private XRBaseController _rightHand;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _timeBetweenShots;
+
+    private XRGrabInteractable _interactable;
+
+    [SerializeField]
+    private HapticInteractable _haptic;
+
     [field:SerializeField] 
     public int Ammo { get; private set; } = 10;
     [field:SerializeField] 
     public int MaxAmmo { get; private set; } = 20;
 
 
+    private void Awake()
+    {
+        _interactable = GetComponentInParent<XRGrabInteractable>();
+        Mathf.Clamp(_timeBetweenShots, 0f, 1f);
+    }
 
-    public void getAmmo(int count){
-        if (count >0){
+
+    private IEnumerator FireMissile()
+    {
+        while (true) 
+        {
+            if(Ammo <= 0)
+            {
+                Mathf.Clamp(Ammo, 0, MaxAmmo);
+                StopCoroutine(FireMissile());
+            }
+            
+            // Set missile
+            GameObject spawnedMissile = Instantiate(_missile);
+            spawnedMissile.transform.position = _spawnPoint.transform.position;
+            spawnedMissile.GetComponent<Rigidbody>().velocity = _spawnPoint.transform.forward * _fireSpeed;
+            Destroy(spawnedMissile, 5);
+
+            _haptic.TriggerHaptic();
+                        
+            Ammo--;
+
+            yield return new WaitForSeconds(_timeBetweenShots);
+        }
+    }
+
+
+    public void StartFire(ActivateEventArgs arg)
+    {
+        if (Ammo >= 0 && _interactable.interactorsSelecting.Count == 2)
+            StartCoroutine(FireMissile());
+    }
+
+    public void StopFire(DeactivateEventArgs arg) 
+    { 
+        StopCoroutine(FireMissile());
+    }
+
+    public void getAmmo(int count)
+    {
+        if (count > 0)
+        {
             Debug.Log("Got Ammo");
             Ammo += count;
         }
-        if (Ammo > MaxAmmo){
+        if (Ammo > MaxAmmo)
+        {
             Ammo = MaxAmmo;
         }
     }
 
-    public void FireMissile(ActivateEventArgs arg)
-    {
-        if (Ammo >= 0){
-            GameObject spawnedMissile = Instantiate(_missile);
 
-            spawnedMissile.transform.position = _spawnPoint.transform.position;
-            spawnedMissile.GetComponent<Rigidbody>().velocity = _spawnPoint.transform.forward * _fireSpeed;
-
-            Destroy(spawnedMissile, 5);
-            Ammo--;
-        }
-    }
 }
